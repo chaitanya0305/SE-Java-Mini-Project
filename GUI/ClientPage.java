5f9a3a2f04524365e7bcf0694eabdb8107999e33
Rudrani26 <66739928+Rudrani26@users.noreply.github.com>
2022-10-29 00:18:57 +0530

    Update ClientPage.java

--- GUI/ClientPage.java
+++ GUI/ClientPage.java
@@ -13,12 +13,12 @@ import javax.swing.JOptionPane;
  *
  * @author rudra
  */
-public class ClientServer extends javax.swing.JFrame {
+public class ClientPage extends javax.swing.JFrame {
 
     /**
      * Creates new form ClientServer
      */
-    public ClientServer() {
+    public ClientPage() {
         initComponents();
     }
 
@@ -318,20 +318,21 @@ public class ClientServer extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ClientServer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ClientServer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ClientServer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ClientServer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ClientServer().setVisible(true);
+                new ClientPage().setVisible(true);
             }
         });
     }
